5fe58b8c595883334d6076bb4da710c436ecc8df
Utkarsh <utkarsh7213@gmail.com>
2024-04-12 19:05:45 +0530

    nextjs top loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
+import NextTopLoader from "nextjs-toploader";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+    <NextTopLoader
+        color="#2a3890"
+        showSpinner={false}
+        zIndex={999}
+        height={4}
+      />
       <ToastContainer hideProgressBar={true} />
 
       <Component {...pageProps} />
