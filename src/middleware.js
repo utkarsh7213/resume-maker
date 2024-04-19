2f1474bf83539216c29fa56cbca4d4d64ef24be0
Utkarsh <utkarsh7213@gmail.com>
2024-04-19 22:18:51 +0530

    fix

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/", '/template/:path*'] };
+export const config = { matcher: ["/", '/template/:path*',"/contact", "/about"] };
